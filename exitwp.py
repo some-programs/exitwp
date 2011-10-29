2c384be102db1b8f45ccef597dab9d686437b9f9
Yu-Cheng Chuang <ducksteven@gmail.com>
2011-10-30 07:24:46 +0800

    use html2text.py instead

--- exitwp.py
+++ exitwp.py
@@ -12,6 +12,7 @@ import tempfile
 from BeautifulSoup import BeautifulSoup
 from urlparse import urlparse, urljoin
 from urllib import urlretrieve
+from html2text import html2text
 
 '''
 exitwp - Wordpress xml exports to Jekykll blog format conversion
@@ -40,17 +41,7 @@ def html2fmt(html, target_format):
         return html
     else:
         # This is like very stupid but I was having troubles with unicode encodings and process.POpen
-        f=codecs.open('pandoc.in', 'w', encoding='utf-8')
-        f.write(html)
-        f.close()
-        call(["pandoc","-f","html","-o", "pandoc.out", "-t",target_format, "pandoc.in"])
-        f=codecs.open('pandoc.out', 'r', encoding='utf-8')
-        lines=[]
-        for line in f: lines.append(line)
-        f.close()
-        os.remove('pandoc.in')
-        os.remove('pandoc.out')
-        return ''.join(lines)
+        return html2text(html, '')
 
 def parse_wp_xml(file):
     ns = {
